002c5a3ca3b27f4002865057ea17fafbb901cd83
gCode2 <g.libront@gmail.com>
2023-02-01 12:09:20 +0100

    Finalna wersja projektu - strona WEB

--- buk_web/src/main/java/buk_web/LoginBB.java
+++ buk_web/src/main/java/buk_web/LoginBB.java
@@ -1,7 +1,14 @@
-package com.buk.login;
+package buk_web;
 
 
-//import javax.enterprise.context.RequestScoped;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.simplesecurity.RemoteClient;
@@ -11,13 +18,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import com.buk.dao.UserDAO;
+import com.buk.dao.WalletDAO;
 import com.buk.entities.User;
+import com.buk.entities.Wallet;
 
 @Named
-//@RequestScoped
+@RequestScoped
 public class LoginBB {
-	private static final String PAGE_MAIN = "/pages/calc/calc?faces-redirect=true";
-	private static final String PAGE_LOGIN = "/pages/login";
+	private static final String PAGE_MAIN = "/pages/buk/index.jsf?faces-redirect=true";
+	private static final String PAGE_LOGIN = "/pages/login.jsf?faces-redirect=true";
 	private static final String PAGE_STAY_AT_THE_SAME = null;
 
 	private String login;
@@ -39,43 +48,53 @@ public class LoginBB {
 		this.pass = pass;
 	}
 
+	public String loginPage() {
+		return "/pages/login?faces-redirect=true";
+	}
+	
 	@Inject
 	UserDAO userDAO;
+	@Inject
+	WalletDAO walletDAO;
 
-	public String doLogin() {
+	public String doLogin() throws IOException, InterruptedException{
 		FacesContext ctx = FacesContext.getCurrentInstance();
 
 		// 1. verify login and password - get User from "database"
 		User user = userDAO.getUserFromDatabase(login, pass);
-
+		
 		// 2. if bad login or password - stay with error info
 		if (user == null) {
-			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+			ctx.addMessage("username", new FacesMessage(FacesMessage.SEVERITY_ERROR,
 					"Niepoprawny login lub hasło", null));
 			return PAGE_STAY_AT_THE_SAME;
 		}
-
 		// 3. if logged in: get User roles, save in RemoteClient and store it in session
 		
 		RemoteClient<User> client = new RemoteClient<User>(); //create new RemoteClient
 		client.setDetails(user);
+		//ZAPISZ DANE PORTFELA DO SESJI
+		Wallet uWallet = walletDAO.getWalletFromDatabase(user.getId());
+		client.getRoles().add(user.getRole());
 		
-		
-	
 		//store RemoteClient with request info in session (needed for SecurityFilter)
 		HttpServletRequest request = (HttpServletRequest) ctx.getExternalContext().getRequest();
 		client.store(request);
-
-		// and enter the system (now SecurityFilter will pass the request)
+		HttpSession session = (HttpSession) ctx.getExternalContext().getSession(false);
+		session.setAttribute("uWallet", uWallet);
 		return PAGE_MAIN;
 	}
+	
 	
 	public String doLogout(){
 		HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
 				.getExternalContext().getSession(true);
-		//Invalidate session
-		// - all objects within session will be destroyed
-		// - new session will be created (with new ID)
+		
+		FacesContext ctx = FacesContext.getCurrentInstance();
+		
+		ctx.addMessage("logoutConfirmation", new FacesMessage(FacesMessage.SEVERITY_INFO,
+				"Wylogowano pomyślnie", null));
+		
 		session.invalidate();
 		return PAGE_LOGIN;
 	}
